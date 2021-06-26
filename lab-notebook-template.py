32b6f5652d468eb78047e07548613cc01a229f0f
Katherine Walden <katie.walden@hotmail.com>
2021-06-25 21:14:36 -0400

    Add files via upload

---
+++ lab-notebook-template.py
@@ -0,0 +1,324 @@
+# Your Name
+# Your email or NetID
+
+# Python Conditional Statements and Functions
+
+"""Q1: Provide your code + comments for steps 37-42."""
+
+
+
+"""Q2: Describe how you approached writing the program for Q1."""
+
+
+
+"""Q3: Return to the program you wrote for Q1 and Q2. Modify this program so it prints one message if the user enters your favorite color, and a different message if not. In addition, your program should print a final message for every user, regardless of what they entered. Include code + comments."""
+
+
+
+"""Q4: Explain what this program will output and why.
+
+fruits = ['apple', 'pear', 'orange']
+
+#a test to see if apple is on the list
+if 'apple' in fruits:
+  print('Would you like an apple?')
+else:
+  print('Sorry, we are all out of apples.')
+
+#a test to see if banana is NOT on the list
+if 'banana' not in fruits:
+  print('Yes, we have no bananas')
+else:
+  print('Would you like a banana?')
+"""
+
+"""Q5: What do you think will happen if we write a `for` loop for `numlist`? What would be a single element in the list?
+
+numlist = [[0, 42], [67543, -987654]]
+"""
+
+"""Q6: What do you expect this code to output? Explain how this program works in your own words.
+
+characters = ['Arya', ' Benjen', 'Bran', 'Catelyn', 'Eddard', 'Rickon', 'Robb', 'Sansa']
+
+print('Members of the House Stark:')
+
+for character in characters:
+  print(character.title() + " Stark")
+
+print(characters[0].title() + " is my favorite.")
+"""
+
+"""Q7: Describe how you would start building out code to accomplish this task? What functions, statements, or keywords would you need to use? How would you start to organize this program?"""
+
+
+
+
+"""Q8: See where you can get with writing this program. What parts of the program were you able to get working? Where did you run into challenges?"""
+
+
+
+
+"""Q9: How does the sample program compare to your approach to the previous two questions? What was similar? What was different? How are you thinking differently (if at all) about how to approach this type of program?"""
+
+
+
+
+"""Q10: Describe how you would start building out code to accomplish this task? What functions, statements, or keywords would you need to use? How would you start to organize this program?"""
+
+
+
+
+"""Q11: See where you can get with writing this program. What parts of the program were you able to get working? Where did you run into challenges?"""
+
+
+
+
+"""Q12: How does the sample program compare to your approach to the previous two questions? What was similar? What was different? How are you thinking differently (if at all) about how to approach this type of program?"""
+
+
+
+"""Q13: What is the value of n in the first iteration of the loop? What happens to the value of n in each iteration of the loop?
+
+n = 0
+
+while n < 10:
+ print(n)
+ n = n +1
+"""
+
+
+"""Q14: What is the endpoint for this loop, or when will this loop end?"""
+
+
+
+"""Q15: Now it's your turn. Describe what is happening in each line of this program. You can use the explanations for the previous example as a model.
+
+# assign x variable
+x = 10
+
+# looping structure using greater than or equal to conditional statement
+while x >= 0:
+ print(x)
+ x = x-1
+
+# message to print once loop has completed
+print('I'm done!')
+"""
+
+
+"""Q16: What is the value of x in the first iteration of the loop? What happens to the value of x in each iteration of the loop?"""
+
+
+
+"""Q17: What is the endpoint for this loop, or when will this loop end?"""
+
+
+
+"""Q18: What will happen if you remove the statement count = count + 1 from this program? Remove the statement to verify your prediction."""
+
+
+
+
+"""Q19: Modify the program from step 141 so that it prints some word or phrase nine times, and also prints a line number at the beginning of each line. Answer will include code + comments.
+
+count = 1
+while count <= 5:
+   print ("Notre")
+   print ("Dame")
+   count = count + 1
+print ("Done")
+"""
+
+
+"""Q20: Now it's your turn. Describe what is happening in each line of this program. You can use explanations for previous examples as a model.
+
+# assign i variable
+i = 1
+
+# while loop
+while i < 200:
+ if i > 150:
+  print('Expression value is greater than 150')
+ elif i > 100:
+  print('Expression value is greater than 100')
+ elif i > 50:
+  print('Expression value is greater than 50')
+ elif i < 50:
+  print('Expression value is less than 50')
+ i += 10
+
+print("That's all, folks!")
+"""
+
+"""Q21: What is the value of i in the first iteration of the loop? What happens to the value of i in each iteration of the loop?
+
+# assign i variable
+i = 1
+
+# while loop
+while i < 200:
+ if i > 150:
+  print('Expression value is greater than 150')
+ elif i > 100:
+  print('Expression value is greater than 100')
+ elif i > 50:
+  print('Expression value is greater than 50')
+ elif i < 50:
+  print('Expression value is less than 50')
+ i += 10
+
+print("That's all, folks!")
+"""
+
+"""Q22: What is the endpoint for this loop, or when will this loop end?"""
+
+
+
+"""Q23: Describe what would happen in each iteration of this loop. How many iterations would it take for the break statement to come into effect?
+
+# assign i variable 
+i = 1
+
+# while loop
+while i < 6:
+ print(i)
+ if i == 3:
+  break
+ i += 1
+"""
+ 
+"""Q24: Describe what would happen in each iteration of this loop. How many iterations would it take for the break statement to come into effect?
+
+# assign the i variable
+i = 0
+
+# while loop
+while i < 6:
+ i += 1
+ if i ==3:
+  continue
+ print(i)
+"""
+
+"""Q25: What is the endpoint for this loop, or when will this loop end?"""
+
+
+
+"""Q26: Write a loop that prints out all numbers from 0 to 10. Include your code + comments that document how the code works."""
+
+
+
+"""Q27: Convert the following Python code to use a while loop instead of a for loop. Include your modified code with comments.
+
+numbers = [5, 4, 7, 0, 1]
+count   = 0
+
+for number in numbers:
+    if not number:
+        break
+    count += 1
+
+print(count)
+"""
+
+"""Q28: Write a program that prints a list of the first nine positive integers and their squares. Include code + comments."""
+
+
+
+"""Q29: Write a program that counts from 10 down to 1, and then prints "Blastoff!""""
+
+
+
+"""Q30: Write a program that asks the user to enter three numbers: a starting value, an ending value, and an increment. Your program should then "count" based on these criteria, as shown in the sample output below. Include code + comments."""
+
+
+
+"""Q31: Write a program that uses a `while` loop to print the output shown below. Include code + comments. The program should print the numbers 1 through 10, except that between 7 and 8 it should print the word "happy". This can be accomplished by placing an `if` statement within the `while` loop to do something special for the `lineNumber == 7` case.
+
+# sample output
+1
+2
+3
+4
+5
+6
+7
+happy
+8
+9
+10
+"""
+
+"""Q32: Write a program that plays a familiar guessing game.
+- First your program will set some number to be the "correct answer"
+- Then your program will ask the user to guess this answer until they get it right.
+- For each guess your program should give a hint based on whether the guess was too high or too low."""
+
+
+
+"""Q33: Write a program that reads in three strings and prints them out in alphabetical order. Include code + comments."""
+
+
+
+"""Q34: In your own words, what is iteration?"""
+
+
+
+"""Q35: In your own words, what is the difference between a for loop and a while loop?"""
+
+
+
+"""Q36: Describe how you would start building out code to accomplish this task? What functions, statements, or keywords would you need to use? How would you start to organize this program?"""
+
+
+
+"""Q37: See where you can get with writing this program. What parts of the program were you able to get working? Where did you run into challenges?"""
+
+
+
+"""Q38: How does the sample program compare to your approach to the previous two questions? What was similar? What was different? How are you thinking differently (if at all) about how to approach this type of program?"""
+
+
+
+"""Q39: How would you write a program that calls the function you have created? Include code + comments."""
+
+
+
+
+"""Q40: Write a function is_even that determines whether or not a number n is even. Include code + comments for the function definition as well as a sample function call."""
+
+
+
+
+"""Q41: Write a function average that determines the average value of a list. Include code + comments for the function definition as well as a sample function call."""
+
+
+
+
+"""Q42: Write a function uniq that takes a list and returns a new list containing only unique values. Include code + comments for the function definition as well as a sample function call."""
+
+
+
+
+"""Q43: The Python code below is suppose to create a function that determines if the given list of numbers is sorted. That is, the function should return True if each item in the list is less than the next item. Unfortunately, there are a few errors in the code below. Identify the errors and fix the code. Include your modified code with comments that document what changes you made to address the errors.
+
+is_sorted([1, 2, 3, 4, 2])
+
+def is_sorted(numbers):
+    ''' Return whether or not the list of numbers is sorted '''
+    for i in range(len(numbers) - 1):
+        if numbers[i] > numbers[i + 1]:
+            return False
+    return True
+"""
+
+"""Q44: In your own words, what is a function?"""
+
+"""Q45: In your own words, how do we create a function?"""
+
+"""Q46: In your own words, what is an argument or parameter?"""
+
+"""Q47: In your own words, what is a return value?"""
+
+"""Q48: Why would we use functions, or what is the value of functions?"""
